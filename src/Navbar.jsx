3d5b9b186defa61584e61d6365f8b655953623da
andrewpersaud1 <andrewpersaud022@gmail.com>
2023-10-31 16:38:12 -0400

    first push

---
+++ src/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Navbar(){
+    return(
+        <div className="Top">
+            <h1>Overload</h1>
+            <h2>About</h2>
+            <h2>Contact</h2>
+            <h2>exercise</h2>
+        </div>
+    )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/App.css",
+    "./src/index.css",
+    "./src/Navbar.css",
+    "./src/Navbar.jsx",
+    "./src/main.jsx",
+    "./src/App.jsx"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
