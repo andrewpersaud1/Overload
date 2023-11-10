8d0da1cac9ce5ee6347a53db1031e5f50003df78
andrewpersaud1 <andrewpersaud022@gmail.com>
2023-11-10 14:27:17 -0500

    images updated

--- src/data.jsx
+++ src/data.jsx
@@ -2,7 +2,7 @@ export default [
     {
         id: 1,
         title: "Bench Press",
-        coverImg:"benchpress.jpg" ,
+        coverImg:"armoredtitanbenching.png" ,
         description: "The bench press is a popular strength training exercise that primarily targets the muscles of the chest, shoulders, and triceps. It is a compound movement, meaning it involves multiple joints and muscle groups working together. ",
         importance: "strength important",
         stats: {
@@ -14,7 +14,7 @@ export default [
     {
         id: 1,
         title: "Squat",
-        coverImg:"squat.jpg" ,
+        coverImg:"animesquat.png" ,
         description: "The squat is a fundamental strength training exercise that targets multiple muscle groups in the lower body and core. It is considered one of the most effective compound exercises for building overall strength and power.",
         importance: "strength, and muscle building",
         stats: {
@@ -26,7 +26,7 @@ export default [
     {
         id: 1,
         title: "Deadlift",
-        coverImg:"deadlift.jpg" ,
+        coverImg:"supervegetadeadlifting.png" ,
         description: " The deadlift is a compound strength training exercise that involves lifting a weight (typically a barbell) from the ground to a standing position. It's considered one of the most effective exercises for developing overall strength and targeting a wide range of muscle groups.",
         importance: "strength, and muscle building",
         stats: {
