aeba48bff3981c681b25428299712ab99d5261d9
andrewpersaud1 <andrewpersaud022@gmail.com>
2023-11-12 19:32:34 -0500

    card changes

--- src/Exercise.jsx
+++ src/Exercise.jsx
@@ -3,15 +3,16 @@ import React from "react"
 export default function Exercise(props){
     
     return(
-        <div className="block">
-            <h1>Title: {props.item.title}</h1>
+        <div className="block-container">
+            <div className="block">
+            <h1 className="block-title">Title: {props.item.title}</h1>
             <img src={`./images/${props.item.coverImg}`} width="100px" className="block-image"/>
           <div className="block-stats">
                 <h2>Description: {props.item.description}</h2>
                 <h2>muscles used:{props.item.stats.targetmuscle}</h2>
                 <h2>difficulty: {props.item.stats.difficulty}</h2>
                 <h2>Risk: {props.item.stats.Risk}</h2>
-                
+            </div>
           </div>
         </div>
     )
