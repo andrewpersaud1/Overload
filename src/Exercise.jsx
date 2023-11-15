9d35ca7349242a7a4832c2d5665573857d296f20
andrewpersaud1 <andrewpersaud022@gmail.com>
2023-11-14 20:04:49 -0500

    card changed

--- src/Exercise.jsx
+++ src/Exercise.jsx
@@ -4,16 +4,13 @@ export default function Exercise(props){
     
     return(
         <div className="block-container">
-            <div className="block">
-            <h1 className="block-title">Title: {props.item.title}</h1>
+            
             <img src={`./images/${props.item.coverImg}`} width="100px" className="block-image"/>
-          <div className="block-stats">
+            <h2 className="block-title">Title: {props.item.title}</h2>
                 <h2>Description: {props.item.description}</h2>
                 <h2>muscles used:{props.item.stats.targetmuscle}</h2>
                 <h2>difficulty: {props.item.stats.difficulty}</h2>
                 <h2>Risk: {props.item.stats.Risk}</h2>
             </div>
-          </div>
-        </div>
     )
 }
